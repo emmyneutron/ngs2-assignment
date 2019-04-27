7da9a01898622632ae3e2c660099e4803f1c1221
Eman Abdelrazik <eman50371@student.must.edu.eg>
2019-04-27 02:01:43 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,1 +1,18 @@
+#Alignment with STAR2
+## STAR installation
+sudo apt-get update
+sudo apt-get install g++
+sudo apt-get install make
+sudo apt install rna-star
 
+#1 chr22 indexing
+cd ~/workdir/assignment/ngs2_assignment
+mkdir $genomeDir
+genomeDir=~/workdir/assignment/ngs2_assignment/chr22_with_ERCC92.fa
+STAR --runMode genomeGenerate --genomeDir $genomeDir --chr22_with_ERCC92.fa\ --runThreadN 1
+
+#2 alignment
+runDir=/path/to/1pass
+mkdir $runDir
+cd $runDir
+STAR --genomeDir $genomeDir --readFilesIn mate1.fq mate2.fq --runThreadN 1
