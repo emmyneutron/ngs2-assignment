34c099781bbb2a625809091b610303aebe0a4dda
Eman Abdelrazik <eman50371@student.must.edu.eg>
2019-04-27 15:18:25 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -8,11 +8,8 @@ sudo apt install rna-star
 #1 chr22 indexing
 cd ~/workdir/assignment/ngs2_assignment
 mkdir $genomeDir
-genomeDir=~/workdir/assignment/ngs2_assignment/chr22_with_ERCC92.fa
-STAR --runMode genomeGenerate --genomeDir $genomeDir --chr22_with_ERCC92.fa\ --runThreadN 1
+genomeDir=~/workdir/assignment/ngs2_assignment/genomeDir/GRCh38.primary_assembly.genome.fa
+STAR --runMode genomeGenerate --genomeDir $genomeDir --genomeFastaFiles GRCh38.primary_assembly.genome.fa\ --runThreadN 1
 
 #2 alignment
-runDir=/path/to/1pass
-mkdir $runDir
-cd $runDir
-STAR --genomeDir $genomeDir --readFilesIn mate1.fq mate2.fq --runThreadN 1
+
