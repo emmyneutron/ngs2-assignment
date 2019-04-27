248f4fc8259b4716c73ef52987fedb48945283d0
Eman Abdelrazik <eman50371@student.must.edu.eg>
2019-04-27 18:22:39 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -6,10 +6,20 @@ sudo apt-get install make
 sudo apt install rna-star
 
 #1 chr22 indexing
-cd ~/workdir/assignment/ngs2_assignment
-mkdir $genomeDir
-genomeDir=~/workdir/assignment/ngs2_assignment/genomeDir/GRCh38.primary_assembly.genome.fa
-STAR --runMode genomeGenerate --genomeDir $genomeDir --genomeFastaFiles GRCh38.primary_assembly.genome.fa\ --runThreadN 1
+mkdir ~/workdir/assignment/ngs2_assignment/genome && cd ~/workdir/assignment/ngs2_assignment/genome
+mkdir idx/
+#STAR --runThreadN 1 --runMode genomeGenerate --genomeDir idx/ --genomeFastaFiles GRCh38.primary_assembly.genome.fa ##core dumped
+STAR --runThreadN 1 --runMode genomeGenerate --genomeDir idx/ --genomeFastaFiles chr22_with_ERCC92.fa
 
-#2 alignment
+#2 data preparation
+mkdir ~/workdir/assignment/ngs2_assignment/sample_data && cd ~/workdir/assignment/ngs2_assignment/sample_data
+wget https://ufile.io/kc0qqbvd #download it directly from the website
+unzip ngs2-assignment-data.zip|ls
+cd ngs2-assignment-data/
+ls
+gunzip SRR8797509*.fastq.gz
+ls
+cd ..
+mate1=~/workdir/assignment/ngs2_assignment/sample_data/ngs2-assignment-data/SRR8797509_1.part_001.part_001.fastq.
+mate2=~/workdir/assignment/ngs2_assignment/sample_data/ngs2-assignment-data/SRR8797509_2.part_001.part_001.fastq.
 
